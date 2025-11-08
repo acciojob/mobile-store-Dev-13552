177361f85427f0fd39afeef00920a4c51eefd9ec
Dev-13552 <devrathour110@gmail.com>
2025-11-08 22:32:38 +0530

    c6

--- src/Pages/AdminPanel.js
+++ src/Pages/AdminPanel.js
@@ -28,32 +28,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const AdminPanel = ({ filterData, setFilterData }) => {
-  const deleteHandler = (id) => {
-    setFilterData((prev) => prev.filter((item) => item.id !== id));
-  };
+  function deleteHandler(id) {
+    setFilterData((prevData) => prevData.filter((item) => item.id !== id));
+  }
 
   return (
     <div className="container">
-      <Link to="/admin/add" className="btn add-btn">
+      <Link to="/admin/add" className="btn add-btn mb-3">
         Add product
       </Link>
 
-      {/* each product row */}
+      {/* render each product as Bootstrap-like row */}
       {filterData.map((item) => (
-        <div key={item.id} className="col-12">
+        <div key={item.id} className="col-12 mb-2">
           <div>
-            {/* this matches div.col-12>div>a */}
+            {/* EXACT structure Cypress expects: div.col-12 > div > a */}
             <Link to={`/admin/products/${item.id}`}>
-              {/* this matches :nth-child(4)>a>.row */}
+              {/* child .row so :nth-child(4) > a > .row matches */}
               <div className="row">
                 <div className="col-6">{item.title}</div>
-                <div className="col-6">{item.price}</div>
+                <div className="col-6 text-right">${item.price}</div>
               </div>
             </Link>
           </div>
 
+          {/* Delete button (Cypress may click .float-right) */}
           <button
-            className="btn delete-btn float-right"
+            className="btn delete-btn float-right mt-1"
             onClick={() => deleteHandler(item.id)}
           >
             Delete
@@ -65,4 +66,3 @@ const AdminPanel = ({ filterData, setFilterData }) => {
 };
 
 export default AdminPanel;
-
