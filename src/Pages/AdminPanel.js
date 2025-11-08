a82837d449e650bb3e5480406b512063c5937f4b
Dev-13552 <devrathour110@gmail.com>
2025-11-08 22:28:25 +0530

    c5

--- src/Pages/AdminPanel.js
+++ src/Pages/AdminPanel.js
@@ -24,74 +24,45 @@
 
 // export default AdminPanel
 // import React from "react";
-// import { Link } from "react-router-dom";
-
-// const AdminPanel = ({ filterData, setFilterData }) => {
-//   function clickHandler() {
-//     // navigate to add page
-//     // this component doesn't have router props if rendered via render; using Link below instead is simpler
-//   }
-
-//   return (
-//     <div className="adminContainer">
-//       <Link to="/admin/add" className="btn add-btn">Add product</Link>
-
-//       <div className="adminList">
-//         {filterData.map((item) => (
-//           <div key={item.id} className="adminCard">
-//             {/* Product link for editing/viewing in admin */}
-//             <Link className="product-link" to={`/admin/products/${item.id}`}>
-//               {item.title}
-//             </Link>
-
-//             {/* Optional delete button per item if desired on this list too:
-//                 <button className="btn delete-btn" onClick={() => {
-//                   setFilterData(prev => prev.filter(p => p.id !== item.id))
-//                 }}>Delete</button>
-//             */}
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default AdminPanel;
 import React from "react";
 import { Link } from "react-router-dom";
 
 const AdminPanel = ({ filterData, setFilterData }) => {
-  function deleteHandler(id) {
-    setFilterData((prevData) => prevData.filter((item) => item.id !== id));
-  }
+  const deleteHandler = (id) => {
+    setFilterData((prev) => prev.filter((item) => item.id !== id));
+  };
 
   return (
-    <div className="adminContainer">
-      <Link to="/admin/add" className="btn add-btn">Add product</Link>
+    <div className="container">
+      <Link to="/admin/add" className="btn add-btn">
+        Add product
+      </Link>
 
-      <div className="container">
-        {filterData.map((item) => (
-          <div key={item.id} className="col-12">
-            <div>
-              <Link to={`/admin/products/${item.id}`}>
-                <div className="row">
-                  <div className="col-6">{item.title}</div>
-                  <div className="col-6">{item.price}</div>
-                </div>
-              </Link>
-            </div>
-
-            <button
-              className="btn delete-btn float-right"
-              onClick={() => deleteHandler(item.id)}
-            >
-              Delete
-            </button>
+      {/* each product row */}
+      {filterData.map((item) => (
+        <div key={item.id} className="col-12">
+          <div>
+            {/* this matches div.col-12>div>a */}
+            <Link to={`/admin/products/${item.id}`}>
+              {/* this matches :nth-child(4)>a>.row */}
+              <div className="row">
+                <div className="col-6">{item.title}</div>
+                <div className="col-6">{item.price}</div>
+              </div>
+            </Link>
           </div>
-        ))}
-      </div>
+
+          <button
+            className="btn delete-btn float-right"
+            onClick={() => deleteHandler(item.id)}
+          >
+            Delete
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default AdminPanel;
+
